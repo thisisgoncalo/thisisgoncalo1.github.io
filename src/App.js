d637a2cc1b31b54e0bd23c637749e2f6059cbcd2
thisisgoncalo <goncalomgmt@gmail.com>
2024-07-08 21:11:21 +0100

    join goncalo club extension 4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import JoinForm from './components/JoinForm';
 import ThankYouPage from './components/ThankYouPage';
+import RedirectComponent from './components/RedirectComponent'; // Import the new component
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/join-form" element={<JoinForm />} />
         <Route path="/joingoncaloclub" element={<JoinForm />} />
+        <Route path="/redirect-joingoncaloclub" element={<RedirectComponent />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
       </Routes>
     </Router>
